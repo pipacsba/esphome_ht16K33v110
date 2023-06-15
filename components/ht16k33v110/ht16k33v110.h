2a9a23d46f6501a9e5560427cfa9ea7e2e0f833a
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-15 09:55:07 +0200

    Update ht16k33v110.h

--- components/ht16k33v110/ht16k33v110.h
+++ components/ht16k33v110/ht16k33v110.h
@@ -46,10 +46,12 @@ class HT16K33V110Display : public PollingComponent {
  
   void display();
 
+#ifdef USE_TIME
   /// Evaluate the strftime-format and print the result at the given position.
-  uint8_t strftime(uint8_t pos, const char *format, ESPTime time) __attribute__((format(strftime, 3, 0)));
+  uint8_t strftime(uint8_t pos, const char *format, time::ESPTime time) __attribute__((format(strftime, 3, 0)));
   /// Evaluate the strftime-format and print the result at position 0.
-  uint8_t strftime(const char *format, ESPTime time) __attribute__((format(strftime, 2, 0)));
+  uint8_t strftime(const char *format, time::ESPTime time) __attribute__((format(strftime, 2, 0)));
+#endif
 
  protected:
   void bit_delay_();
