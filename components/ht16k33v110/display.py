340b32620791c6c43bdd27be153fdf7147010d9a
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-16 20:41:42 +0200

    Update display.py

--- components/ht16k33v110/display.py
+++ components/ht16k33v110/display.py
@@ -81,7 +81,14 @@ async def to_code(config):
     cg.add(var.set_intensity(config[CONF_INTENSITY]))
     cg.add(var.set_inverted(config[CONF_INVERTED]))
     if CONF_INTENSITY_MAP in config:
-        cg.add(var.set_intensity_map(config[CONF_INTENSITY_MAP][CONF_MAP]))
+        #cg.add(var.set_intensity_map(config[CONF_INTENSITY_MAP][CONF_MAP]))
+        intensity_source_values = []
+        intensity_values = []
+        for a_value in config[CONF_INTENSITY_MAP][CONF_MAP]:
+            intensity_source_values.append(a_value[CONF_SENSOR_VALUE])
+            intensity_values.append(a_value[CONF_SENSOR_VALUE])
+        cg.add(set_intensity_source_values(intensity_source_values)
+        cg.add(set_intensity_values(intensity_values)
         cg.add(var.set_intensity_auto())
         cg.add(var.set_intensity_sensor_id(config[CONF_INTENSITY_MAP][CONF_SOURCE_ID]))
 
