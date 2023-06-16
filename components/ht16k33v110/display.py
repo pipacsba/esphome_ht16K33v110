92704cfd0d602bc60c6c5baab9ebf63bc601056c
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-16 14:52:34 +0200

    Update display.py

--- components/ht16k33v110/display.py
+++ components/ht16k33v110/display.py
@@ -22,6 +22,21 @@ HT16K33V110DisplayRef = HT16K33V110Display.operator("ref")
 ht16k33v110_intensity_sensor_values = []
 ht16k33v110_intensity_values = []
 
+def validate_intensity_map(value):
+    if isinstance(value, list):
+        raise cv.Invalid("It's a list, hurray!")
+        value = cv.string(value)
+        parts = value.split("->")
+        if len(parts) != 2:
+            raise cv.Invalid(" Intensity map parameter must be of form 3000 -> 2")
+        sensor_value = cv.float(parts[0].strip())
+        intensity_value = cv.float(parts[0].strip())
+        ht16k33v110_intensity_sensor_values.append(sensor_value)
+        ht16k33v110_intensity_values.append(intensity_value)
+    else:
+       raise cv.Invalid(" Intensity map parameter must a list")
+    return value
+
 def validate_intensity(config):
     if (CONF_INTENSITY_MAP in config and CONF_INTENSITY in config):
         raise cv.Invalid(
@@ -35,9 +50,8 @@ INTENSITY_MAP_SCHEMA = cv.Schema(
         #cv.Required(CONF_MAP): validate_intensity_map,
     }
 )
-        
-CONFIG_SCHEMA = (
-    #validate_intensity,
+
+KT16K33V110_SCHEMA = cv.Schema(
     display.BASIC_DISPLAY_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(HT16K33V110Display),
@@ -51,6 +65,10 @@ CONFIG_SCHEMA = (
     .extend(i2c.i2c_device_schema(0x70))
 )
 
+CONFIG_SCHEMA = cv.All(
+    cv.ensure_list(KT16K33V110_SCHEMA),
+    validate_intensity,
+)
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
