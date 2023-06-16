2727ec6ee72e4ff8e6f2581697f06aa6b57fa7f4
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-16 22:32:47 +0200

    Update ht16k33v110.h

--- components/ht16k33v110/ht16k33v110.h
+++ components/ht16k33v110/ht16k33v110.h
@@ -48,6 +48,7 @@ class HT16K33V110Display : public PollingComponent, public i2c::I2CDevice {
   void set_intensity_sensor_id(const char* auto_intensity_source) {this->auto_intensity_source_ = auto_intensity_source;}
   void set_intensity_source_values(std::vector<float> intensity_source_values) {this->intensity_source_values_ = intensity_source_values;}
   void set_intensity_values(std::vector<float> intensity_values) {this->intensity_values_ = intensity_values;}
+  void calculate_new_intensity();
  
   void display();
 
