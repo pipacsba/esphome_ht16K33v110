7ce18e35874dfcf15af3a543f9078e05064963ae
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-16 15:32:02 +0200

    Update display.py

--- components/ht16k33v110/display.py
+++ components/ht16k33v110/display.py
@@ -22,7 +22,7 @@ HT16K33V110DisplayRef = HT16K33V110Display.operator("ref")
 ht16k33v110_intensity_sensor_values = []
 ht16k33v110_intensity_values = []
 
-KT16K33V110_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
+KT16K33V110_SCHEMA = (display.BASIC_DISPLAY_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(HT16K33V110Display),
             cv.Optional(CONF_INTENSITY, default=7): cv.All(
@@ -31,7 +31,7 @@ KT16K33V110_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
             cv.Optional(CONF_INVERTED, default=False): cv.boolean,
             #cv.Optional(CONF_INTENSITY_MAP): cv.maybe_simple_value(INTENSITY_MAP_SCHEMA),
         }
-    ).extend(i2c.i2c_device_schema(0x70))
+    )).extend(i2c.i2c_device_schema(0x70))
 #.extend(cv.polling_component_schema("1s"))
 #
 
