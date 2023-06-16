491eb47140bb322f979663a26a9e6deec05d88e6
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-16 12:27:19 +0200

    Update display.py

--- components/ht16k33v110/display.py
+++ components/ht16k33v110/display.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import display, i2c
+from esphome.components import display, i2c, sensor
 from esphome.const import (
     CONF_LAMBDA,
     CONF_INTENSITY,
@@ -12,18 +12,53 @@ from esphome.const import (
 CONF_INTENSITY_MAP = "intensity_map"
 CONF_SOURCE_ID = "source_id"
 CONF_MAP = "map"
+CONF_INTENSITY_VALUES = "intensity_values"
+CONF_SENSOR_VALUES = "sensor_values"
 
 ht16k33v110_ns = cg.esphome_ns.namespace("ht16k33v110")
 HT16K33V110Display = ht16k33v110_ns.class_("HT16K33V110Display", cg.PollingComponent, i2c.I2CDevice)
 HT16K33V110DisplayRef = HT16K33V110Display.operator("ref")
+
+ht16k33v110_intensity_sensor_values = []
+ht16k33v110_intensity_values = []
+
 
 def validate_intensity(config):
     if (CONF_INTENSITY_MAP in config and CONF_INTENSITY in config):
         raise cv.Invalid(
           f"Do not specify {CONF_INTENSITY} when using {CONF_INTENSITY_MAP}"
         )
-
+        
+def validate_intensity_map(config):
+    if isinstance(value, list):
+        value = cv.string(value)
+        parts = value.split("->")
+        if len(parts) != 2:
+            raise cv.Invalid(" Intensity map parameter must be of form 3000 -> 2")
+        sensor_value = cv.float(parts[0].strip())
+        intensity_value = cv.float(parts[0].strip())
+        ht16k33v110_intensity_sensor_values.append(sensor_value)
+        ht16k33v110_intensity_values.append(intensity_value)
+        value = { CONF_SENSOR_VALUES: ht16k33v110_intensity_sensor_values,
+                   CONF_INTENSITY_VALUES: ht16k33v110_intensity_values}
+    else
+       raise cv.Invalid(" Intensity map parameter must be of form 3000 -> 2")
+    return cv.Schema(
+        {
+            cv.Required(CONF_SENSOR_VALUES): cv.ensure_list,
+            cv.Required(CONF_INTENSITY_VALUES): cv.ensure_list,
+        }(value)
+    )
+        
+INTENSITY_MAP_SCHEMA = cv.Schema(
+    {
+        cv.Required(CONF_SOURCE_ID): cv.use_id(sensor.Sensor),
+        cv.Required(CONF_MAP): validate_intensity_map,
+    }
+)
+        
 CONFIG_SCHEMA = (
+    validate_intensity,
     display.BASIC_DISPLAY_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(HT16K33V110Display),
@@ -31,6 +66,7 @@ CONFIG_SCHEMA = (
                 cv.uint8_t, cv.Range(min=1, max=16)
             ),
             cv.Optional(CONF_INVERTED, default=False): cv.boolean,
+            cv.Optional(CONF_INTENSITY_MAP): cv.maybe_simple_value(INTENSITY_MAP_SCHEMA),
         }
     ).extend(cv.polling_component_schema("1s"))
     .extend(i2c.i2c_device_schema(0x70))
